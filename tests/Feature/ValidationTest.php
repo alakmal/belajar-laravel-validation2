1d75aa3ddbbc529a7ce74f83a8792d43aedd64a2
alyouma <alyoumaakmal4@gmail.com>
2025-06-22 08:04:07 +0700

    adding validation laravel

---
+++ tests/Feature/ValidationTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ValidationTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testValidation()
+    {
+
+        $data = [
+            "username" => "admin",
+            "password" => "12345"
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        self::assertTrue($validator->passes());
+        self::assertFalse($validator->fails());
+    }
+
+    public function testError()
+    {
+        $data = [
+            "username" => "",
+            "password" => "12345"
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+        dd($validator->errors()->toJson());
+    }
+}
